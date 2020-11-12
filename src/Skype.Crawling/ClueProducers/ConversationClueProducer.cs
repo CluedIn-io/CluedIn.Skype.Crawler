d1f5974813c14bc8293ab193d8368273b32400c2
Thomas Träff <thomas@traff.dk>
2020-11-12 16:40:04 +0100

    Added stuff for Conversation

---
+++ src/Skype.Crawling/ClueProducers/ConversationClueProducer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using CluedIn.Core.Agent.Jobs;
+using CluedIn.Crawling.Factories;
+using Microsoft.Exchange.WebServices.Data;
+using CluedIn.Core;
+using CluedIn.Core.Data;
+using CluedIn.Crawling.Skype.Core;
+using jdk.nashorn.@internal.ir;
+using CluedIn.Crawling.Skype.Vocabularies;
+using CluedIn.Crawling.Helpers;
+
+namespace CluedIn.Crawling.Skype.ClueProducers
+{
+    public class ConversationClueProducer : BaseClueProducer<Conversation>
+    {
+        private readonly IClueFactory _factory;
+        private readonly AgentJobProcessorState<SkypeCrawlJobData> _state;
+        private readonly ApplicationContext _appContext;
+
+        public ConversationClueProducer(IClueFactory factory, AgentJobProcessorState<SkypeCrawlJobData> state, ApplicationContext appContext)
+        {
+            _factory = factory;
+            _state = state;
+            _appContext = appContext;
+        }
+
+        protected override Clue MakeClueImpl(Conversation input, Guid accountId)
+        {
+            var clue = _factory.Create(EntityType.Mail, input.Id.UniqueId, accountId);
+
+            var data = clue.Data.EntityData;
+
+            if (input.Topic != null)
+            {
+                data.Name = input.Topic;
+                data.DisplayName = input.Topic;
+            }
+            else
+            {
+                data.Name = "No Topic Set";
+                data.DisplayName = "No Topic Set";
+            }
+
+            if (input.LastModifiedTime != null)
+                data.ModifiedDate = input.LastModifiedTime;
+
+            var vocab = new ConversationVocabulary();
+
+            data.Properties[vocab.Importance] = input.Importance.ToString();
+            data.Properties[vocab.Categories] = input.Categories.PrintIfAvailable();
+            data.Properties[vocab.FlagStatus] = input.FlagStatus.PrintIfAvailable();
+            data.Properties[vocab.GlobalCategories] = input.GlobalCategories.PrintIfAvailable();
+            data.Properties[vocab.GlobalFlagStatus] = input.GlobalFlagStatus.PrintIfAvailable();
+            data.Properties[vocab.GlobalHasAttachments] = input.GlobalHasAttachments.PrintIfAvailable();
+            data.Properties[vocab.GlobalHasIrm] = input.GlobalHasIrm.PrintIfAvailable();
+            data.Properties[vocab.GlobalImportance] = input.GlobalImportance.PrintIfAvailable();
+            data.Properties[vocab.GlobalItemClasses] = input.GlobalItemClasses.PrintIfAvailable();
+            data.Properties[vocab.GlobalItemIds] = JsonUtility.Serialize(input.GlobalItemIds);
+            data.Properties[vocab.GlobalLastDeliveryTime] = input.GlobalLastDeliveryTime.PrintIfAvailable();
+            data.Properties[vocab.GlobalMessageCount] = input.GlobalMessageCount.PrintIfAvailable();
+            data.Properties[vocab.GlobalSize] = input.GlobalSize.PrintIfAvailable();
+            data.Properties[vocab.GlobalUniqueRecipients] = input.GlobalUniqueRecipients.PrintIfAvailable();
+            data.Properties[vocab.GlobalUniqueSenders] = input.GlobalUniqueSenders.PrintIfAvailable();
+            data.Properties[vocab.GlobalUniqueUnreadSenders] = input.GlobalUniqueUnreadSenders.PrintIfAvailable();
+            data.Properties[vocab.GlobalUnreadCount] = input.GlobalUnreadCount.PrintIfAvailable();
+            data.Properties[vocab.HasIrm] = input.HasIrm.PrintIfAvailable();
+            data.Properties[vocab.HasAttachments] = input.HasAttachments.PrintIfAvailable();
+            data.Properties[vocab.InstanceKey] = input.InstanceKey.PrintIfAvailable();
+            data.Properties[vocab.IsDirty] = input.IsDirty.PrintIfAvailable();
+            data.Properties[vocab.IsNew] = input.IsNew.PrintIfAvailable();
+            data.Properties[vocab.ItemClasses] = input.ItemClasses.PrintIfAvailable();
+            data.Properties[vocab.ItemIds] = JsonUtility.Serialize(input.ItemIds);
+            data.Properties[vocab.LastDeliveryTime] = input.LastDeliveryTime.PrintIfAvailable();
+            data.Properties[vocab.LastModifiedTime] = input.LastModifiedTime.PrintIfAvailable();
+            data.Properties[vocab.MessageCount] = input.MessageCount.PrintIfAvailable();
+            data.Properties[vocab.Size] = input.Size.PrintIfAvailable();
+            data.Properties[vocab.Topic] = input.Topic.PrintIfAvailable();
+            data.Properties[vocab.UniqueRecipients] = input.UniqueRecipients.PrintIfAvailable();
+            data.Properties[vocab.UniqueSenders] = input.UniqueSenders.PrintIfAvailable();
+            data.Properties[vocab.UniqueUnreadSenders] = input.UniqueUnreadSenders.PrintIfAvailable();
+            data.Properties[vocab.UnreadCount] = input.UnreadCount.PrintIfAvailable();
+
+            return clue;
+        }
+    }
+}
---
+++ src/Skype.Crawling/Vocabularies/ConversationVocabulary.cs
@@ -0,0 +1,87 @@
+﻿using CluedIn.Core.Data;
+using CluedIn.Core.Data.Vocabularies;
+
+namespace CluedIn.Crawling.Skype.Vocabularies
+{
+    public class ConversationVocabulary : SimpleVocabulary
+    {
+        public ConversationVocabulary()
+        {
+            VocabularyName = "Skype Conversation";
+            KeyPrefix = "skype.conversation";
+            KeySeparator = ".";
+            Grouping = EntityType.Mail;
+
+            AddGroup("Skype Conversation Details", group => {
+
+                Importance = group.Add(new VocabularyKey("Importance", VocabularyKeyDataType.Text));
+                Categories = group.Add(new VocabularyKey("Categories", VocabularyKeyDataType.Text));
+                FlagStatus = group.Add(new VocabularyKey("FlagStatus", VocabularyKeyDataType.Text));
+                GlobalCategories = group.Add(new VocabularyKey("GlobalCategories", VocabularyKeyDataType.Text));
+                GlobalFlagStatus = group.Add(new VocabularyKey("GlobalFlagStatus", VocabularyKeyDataType.Text));
+                GlobalHasAttachments = group.Add(new VocabularyKey("GlobalHasAttachments", VocabularyKeyDataType.Boolean));
+                GlobalHasIrm = group.Add(new VocabularyKey("GlobalHasIrm", VocabularyKeyDataType.Boolean));
+                GlobalImportance = group.Add(new VocabularyKey("GlobalImportance", VocabularyKeyDataType.Text));
+                GlobalItemClasses = group.Add(new VocabularyKey("GlobalItemClasses", VocabularyKeyDataType.Text));
+                GlobalItemIds = group.Add(new VocabularyKey("GlobalItemIds", VocabularyKeyDataType.Json));
+                GlobalLastDeliveryTime = group.Add(new VocabularyKey("GlobalLastDeliveryTime", VocabularyKeyDataType.DateTime));
+                GlobalMessageCount = group.Add(new VocabularyKey("GlobalMessageCount", VocabularyKeyDataType.Text));
+                GlobalSize = group.Add(new VocabularyKey("GlobalSize", VocabularyKeyDataType.Text));
+                GlobalUniqueRecipients = group.Add(new VocabularyKey("GlobalUniqueRecipients", VocabularyKeyDataType.Text));
+                GlobalUniqueSenders = group.Add(new VocabularyKey("GlobalUniqueSenders", VocabularyKeyDataType.Text));
+                GlobalUniqueUnreadSenders = group.Add(new VocabularyKey("GlobalUniqueUnreadSenders", VocabularyKeyDataType.Text));
+                GlobalUnreadCount = group.Add(new VocabularyKey("GlobalUnreadCount", VocabularyKeyDataType.Text));
+                HasIrm = group.Add(new VocabularyKey("HasIrm", VocabularyKeyDataType.Text));
+                HasAttachments = group.Add(new VocabularyKey("HasAttachments", VocabularyKeyDataType.Boolean));
+                InstanceKey = group.Add(new VocabularyKey("InstanceKey", VocabularyKeyDataType.Text));
+                IsDirty = group.Add(new VocabularyKey("IsDirty", VocabularyKeyDataType.Boolean));
+                IsNew = group.Add(new VocabularyKey("IsNew", VocabularyKeyDataType.Boolean));
+                ItemClasses = group.Add(new VocabularyKey("ItemClasses", VocabularyKeyDataType.Text));
+                ItemIds = group.Add(new VocabularyKey("ItemIds", VocabularyKeyDataType.Json));
+                LastDeliveryTime = group.Add(new VocabularyKey("LastDeliveryTime", VocabularyKeyDataType.DateTime));
+                LastModifiedTime = group.Add(new VocabularyKey("LastModifiedTime", VocabularyKeyDataType.DateTime));
+                MessageCount = group.Add(new VocabularyKey("MessageCount", VocabularyKeyDataType.Text));
+                Size = group.Add(new VocabularyKey("Size", VocabularyKeyDataType.Text));
+                Topic = group.Add(new VocabularyKey("Topic", VocabularyKeyDataType.Text));
+                UniqueRecipients = group.Add(new VocabularyKey("UniqueRecipients", VocabularyKeyDataType.Text));
+                UniqueSenders = group.Add(new VocabularyKey("UniqueSenders", VocabularyKeyDataType.Text));
+                UniqueUnreadSenders = group.Add(new VocabularyKey("UniqueUnreadSenders", VocabularyKeyDataType.Text));
+                UnreadCount = group.Add(new VocabularyKey("UnreadCount", VocabularyKeyDataType.Text));
+            });
+        }
+
+        public VocabularyKey Importance { get; set; }
+        public VocabularyKey Categories { get; set; }
+        public VocabularyKey FlagStatus { get; set; }
+        public VocabularyKey GlobalCategories { get; set; }
+        public VocabularyKey GlobalFlagStatus { get; set; }
+        public VocabularyKey GlobalHasAttachments { get; set; }
+        public VocabularyKey GlobalHasIrm { get; set; }
+        public VocabularyKey GlobalImportance { get; set; }
+        public VocabularyKey GlobalItemClasses { get; set; }
+        public VocabularyKey GlobalItemIds { get; set; }
+        public VocabularyKey GlobalLastDeliveryTime { get; set; }
+        public VocabularyKey GlobalMessageCount { get; set; }
+        public VocabularyKey GlobalSize { get; set; }
+        public VocabularyKey GlobalUniqueRecipients { get; set; }
+        public VocabularyKey GlobalUniqueSenders { get; set; }
+        public VocabularyKey GlobalUniqueUnreadSenders { get; set; }
+        public VocabularyKey GlobalUnreadCount { get; set; }
+        public VocabularyKey HasIrm { get; set; }
+        public VocabularyKey HasAttachments { get; set; }
+        public VocabularyKey InstanceKey { get; set; }
+        public VocabularyKey IsDirty { get; set; }
+        public VocabularyKey IsNew { get; set; }
+        public VocabularyKey ItemClasses { get; set; }
+        public VocabularyKey ItemIds { get; set; }
+        public VocabularyKey LastDeliveryTime { get; set; }
+        public VocabularyKey LastModifiedTime { get; set; }
+        public VocabularyKey MessageCount { get; set; }
+        public VocabularyKey Size { get; set; }
+        public VocabularyKey Topic { get; set; }
+        public VocabularyKey UniqueRecipients { get; set; }
+        public VocabularyKey UniqueSenders { get; set; }
+        public VocabularyKey UniqueUnreadSenders { get; set; }
+        public VocabularyKey UnreadCount { get; set; }
+    }
+}
